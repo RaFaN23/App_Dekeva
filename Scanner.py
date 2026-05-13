d958c15296c1891e662454ca06b04600454c9b75
javi9234 <jpachecomontes@safareyes.es>
2026-05-13 13:46:43 +0200

    Analizar

--- Scanner.py
+++ Scanner.py
@@ -50,3 +50,31 @@ def analyze_cookies(response):
         print(f"Cookie: {cookie.name}")
         print(f" Secure: {cookie.secure}")
         print(f" HttpOnly: {cookie.has_nonstandard_attr('HttpOnly')}")
+
+def analyze_forms(html):
+
+  print("\n[FORMULARIOS]")
+  
+  soup = BeautifulSoup(html, 'html.parser')
+  forms = soup.find_all('form')
+  
+  if not forms:
+    print("No se encontraron formularios")
+    return
+  
+  for i, form in enumerate(forms, start=1):
+  
+    csrf_found = False
+  
+    for inp in form.find_all('input'):
+      name = inp.get('name', '').lower()
+    
+      if 'csrf' in name or 'token' in name:
+        csrf_found = True
+  
+    print(f"Formulario #{i}")
+  
+    if csrf_found:
+      print(" [OK] Token CSRF detectado")
+    else:
+      print(" [ALERTA] No se detectó token CSRF")
